87dfb9bac3f623badd10dc450acef671382bf67a
annaneo <aneovesky@gmail.com>
2014-10-17 19:11:23 +0200

    getting started

---
+++ panorama.js
@@ -0,0 +1,21 @@
+// create scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//render element
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+
+function render() {
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
+}
+render();
