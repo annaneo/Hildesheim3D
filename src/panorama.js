64a63b89b8a6a71fd898abc14a4d0fa4bcc77696
Julius <julius@mr-fridge.de>
2014-12-27 17:00:21 +0100

    started with loading spinner

--- src/panorama.js
+++ src/panorama.js
@@ -15,9 +15,41 @@ var mouse = { x: 0, y: 0 };
 var targetList = [];
 var hoverIntersected;
 var composer;
+var progress_hack = 0;
 
 
 function startPanorama(panoImg) {
+	if (progress_hack === 0) {
+		var opts = {
+			lines: 15, // The number of lines to draw
+			length: 0, // The length of each line
+			width: 24, // The line thickness
+			radius: 10, // The radius of the inner circle
+			corners: 1, // Corner roundness (0..1)
+			rotate: 0, // The rotation offset
+			direction: 1, // 1: clockwise, -1: counterclockwise
+			color: '#fff', // #rgb or #rrggbb or array of colors
+			speed: 0.7, // Rounds per second
+			trail: 27, // Afterglow percentage
+			shadow: false, // Whether to render a shadow
+			hwaccel: false, // Whether to use hardware acceleration
+			className: 'spinner', // The CSS class to assign to the spinner
+			zIndex: 2e9, // The z-index (defaults to 2000000000)
+			top: '50%', // Top position relative to parent
+			left: '50%' // Left position relative to parent
+		};
+		var target = document.getElementById('loadingScreen');
+		var spinner = new Spinner(opts).spin(target);
+	}
+	if (progress_hack < 100) {
+		progress_hack = progress_hack + 2;
+		setTimeout(function() { startPanorama(panoImg);  }, 20);
+		return;
+	}
+	var loadingScreen = document.getElementById('loadingScreen');
+	loadingScreen.removeChild(loadingScreen.childNodes[0]);
+
+	
 	init(panoImg);
 	animate();
 }
